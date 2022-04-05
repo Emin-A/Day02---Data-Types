3b7fa61f6caf855fa1cf2e2a2d477017dc9d2eac
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-05 15:25:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Data Types
+
+# String 
+
+# print("Hello"[4])
+
+# print("Hello" + "world!")
+
+# Integers
+
+# print(123 + 345)
+
+# Float 
+
+# 3.14159
+
+# Boolean 
+
+# True
+# False
+
+# num_char = len(input("What is your name?"))
+# print("Your name has " + num_char + " characters.")
+# new_num_char = str(num_char)
+
+# print("Your name has " + new_num_char + " characters.") 
+
+a = float(123)
+print(type(a))
+
+# print(70 + float("100.5"))
+print(str(70)+ str(100))
